a7879b82c1275f71aba795499783715fef95a488
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-05-18 14:04:18 -0300

    feat(product): add whatsapp link

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,7 +5,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/AShare.html': path.resolve(__dirname, 'template/js/custom-js/html/AShare.html'),
+      './html/AShare.html': path.resolve(__dirname, 'template/js/custom-js/html/AShare.html')
+      //'./js/AShare.js': path.resolve(__dirname, 'template/js/custom-js/js/AShare.js')
     }
   }
 })
