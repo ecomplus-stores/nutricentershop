a0387a890fc0eb99e4be8ac1535566caa4b8d56b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-05-18 16:25:11 -0300

    chore: add link with whatsapp

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+storefront.on('widget:@ecomplus/widget-minicart', function () {
+    console.log('somos nozes')
+    setTimeout(function () {
+        console.log('entrei')
+        const local = window.location.href
+        const whatsappLink = `https://wa.me/5547996985319?text=Ola%20estou%20na%20sua%20Loja Online. Quero saber mais sobre ${local}`
+        document.getElementById('whatsapp').href = whatsappLink
+    }, 800);
+  });
